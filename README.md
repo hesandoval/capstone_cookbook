8d2d4fa757430e77f2b8541502ba5cc4595db282
Hector Sandoval <hesandova@gmail.com>
2016-03-28 08:54:37 -0700

    Created cookbook files

---
+++ README.md
@@ -0,0 +1,4 @@
+# capstone_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: capstone_cookbook
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+Chef::Log.info("********** Hello, World! **********")
