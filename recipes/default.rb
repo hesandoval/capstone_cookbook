9f510ecb650faff9d8e8e7265003c9e9a2d2efbd
Hector Sandoval <hesandova@gmail.com>
2016-03-28 09:31:30 -0700

    Added app and app_path

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 Chef::Log.info("********** Hello, World! **********")
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+Chef::Log.info(app_path)
