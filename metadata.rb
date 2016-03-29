c95ac97523c59593b722600946b0f69145d7ccc3
Hector Sandoval <hesandova@gmail.com>
2016-03-28 22:39:24 -0700

    changes

--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'capstone_cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures capstone_cookbook'
-long_description 'Installs/Configures capstone_cookbook'
-version '0.1.0'
-
-depends 'application', '5.1.0'
