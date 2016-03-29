7a901c2c52d308b767d9e47cdc794dea9a043f16
Hector Sandoval <hesandova@gmail.com>
2016-03-28 20:23:07 -0700

    Added the rest of the commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-Chef::Log.info("********** Hello, World! **********")
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
-Chef::Log.info("********** Application Path **********"+app_path)
+
+application app_path do
+  javascript "4"
+  environment.update("PORT" => "80")
+
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+  end
+
+  link "#{app_path}/app.js" do
+    to "#{app_path}/index.js"
+  end
+
+  npm_install
+  npm_start
+end
